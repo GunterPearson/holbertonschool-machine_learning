be6aa9bb593bd9d69980d4658bbf9341f852229b
GunterPearson <1715@holbertonschool.com>
2020-11-06 13:48:58 -0600

    made corrections to file 7 for checker

--- math/0x00-linear_algebra/7-gettin_cozy.py
+++ math/0x00-linear_algebra/7-gettin_cozy.py
@@ -5,5 +5,5 @@
 def cat_matrices2D(mat1, mat2, axis=0):
     """ concat matrix based on axis """
     if axis == 0:
-        return mat1.copy() + mat2.copy()
-    return [mat1[idx].copy() + mat2[idx].copy() for idx in range(len(mat1))]
+        return [row.copy() for row in mat1] + [row.copy() for row in mat2]
+    return [mat1[idx] + mat2[idx] for idx in range(len(mat1))]
