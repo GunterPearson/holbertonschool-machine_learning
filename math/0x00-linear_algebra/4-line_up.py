15cddb4f95d95fe1f0c84567f4ce9cc8d03dee9c
GunterPearson <1715@holbertonschool.com>
2020-11-06 12:28:37 -0600

    pushing file 4-line_up

--- math/0x00-linear_algebra/4-line_up.py
+++ math/0x00-linear_algebra/4-line_up.py
@@ -6,5 +6,5 @@ def add_arrays(arr1, arr2):
     """ adds two arrays and returns them"""
     try:
         return [arr1[idx] + arr2[idx] for idx in range(len(arr1))]
-    except:
+    except IndexError:
         return None
