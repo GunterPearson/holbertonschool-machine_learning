ec6ccbaf52a56339925ebe8e948484155cbac0df
GunterPearson <1715@holbertonschool.com>
2020-12-02 13:33:16 -0600

    pushing file 100-slice_like_a_ninja

---
+++ math/0x00-linear_algebra/100-slice_like_a_ninja.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+""" slice like a ninja"""
+
+
+def np_slice(matrix, axes={}):
+    """ slice matrix based on axis given"""
+    form = (max(axes) + 1) * [slice(None)]
+    for k, v in axes.items():
+        form[k] = slice(*v)
+    result = matrix[form]
+    return result
