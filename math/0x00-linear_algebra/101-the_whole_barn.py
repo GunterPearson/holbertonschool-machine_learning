8963ddc55ad50f9cd4766d70ce3a6665a9af01eb
GunterPearson <1715@holbertonschool.com>
2020-12-04 10:28:39 -0600

    pushing file 101-the_whole_barn

--- math/0x00-linear_algebra/101-the_whole_barn.py
+++ math/0x00-linear_algebra/101-the_whole_barn.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python3
 """ the whole barn"""
-import numpy as np
 
 
 def add_matrices(mat1, mat2):
     """ add two matrices"""
-    new = np.array(mat1)
-    second = np.array(mat2)
+    import numpy as np
+    m1 = np.array(mat1)
+    m2 = np.array(mat2)
     try:
-        result = new + second
+        result = m1 + m2
     except ValueError:
         return None
     return result.tolist()
