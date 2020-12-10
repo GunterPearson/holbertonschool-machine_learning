b1ba78cc3a5b4fa5fae86364a52852529fe1ae5b
GunterPearson <1715@holbertonschool.com>
2020-12-10 17:25:13 -0600

    pushing file 17-integrate

--- math/0x02-calculus/17-integrate.py
+++ math/0x02-calculus/17-integrate.py
@@ -6,11 +6,11 @@ def poly_integral(poly, C=0):
     """ integral math"""
     if type(poly) != list or len(poly) == 0:
         return None
-    if type(C) != int or type(C) != float:
+    if type(C) != int and type(C) != float:
         return None
-    for n in poly:
-        if type(n) != int or type(n) != float:
-            return None
+    result = all(type(s) in [int, float] for s in poly)
+    if not result:
+        return None
     poly.insert(0, 0)
     for x in range(1, len(poly)):
         poly[x] = poly[x] / x
