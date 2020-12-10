3ade9d79905a1b634702bc6a0f37d3e5eaf9f7ee
GunterPearson <1715@holbertonschool.com>
2020-12-10 15:58:20 -0600

    pushing file 9-sum

--- math/0x02-calculus/9-sum_total.py
+++ math/0x02-calculus/9-sum_total.py
@@ -6,4 +6,4 @@ def summation_i_squared(n):
     """ squared sum"""
     if n < 1 or type(n) != int:
         return None
-    return sum(map(lambda n : pow(n, 2), range(n + 1)))
+    return sum(map(lambda n: pow(n, 2), range(n + 1)))
