4788a62ca81839b76a10c8758dce933699ff6173
GunterPearson <1715@holbertonschool.com>
2020-12-15 17:14:04 -0600

    pushing file exponential for checker

---
+++ math/0x03-probability/exponential.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+""" exponential"""
+
+
+class Exponential:
+    """ create exponential class"""
+
+    def __init__(self, data=None, lambtha=1.):
+        """ init function for Poisson class"""
+        if data is None:
+            if lambtha <= 0:
+                raise ValueError("lambtha must be a positive value")
+            self.lambtha = float(lambtha)
+        else:
+            if type(data) != list:
+                raise TypeError("data must be a list")
+            if len(data) < 2:
+                raise ValueError("data must contain multiple values")
+            self.lambtha = len(data) / sum(data)
