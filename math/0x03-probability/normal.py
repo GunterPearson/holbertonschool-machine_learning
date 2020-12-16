e1de3640e9e0f956b55fc3c065f0893939d4648c
GunterPearson <1715@holbertonschool.com>
2020-12-16 13:43:16 -0600

    pushing file normal.py for checking

---
+++ math/0x03-probability/normal.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+""" normal class """
+
+
+class Normal:
+    """ creating class Normal"""
+
+    def __init__(self, data=None, mean=0., stddev=1.):
+        """ init function to initialize class """
+        if data is None:
+            if stddev <= 0:
+                raise ValueError("stddev must be a positive value")
+            self.mean = float(mean)
+            self.stddev = float(stddev)
+        else:
+            if type(data) != list:
+                raise TypeError("data must be a list")
+            if len(data) < 2:
+                raise ValueError("data must contain multiple values")
+            self.mean = float(sum(data) / len(data))
+            self.stddev = float((sum(map(lambda n: pow(n - self.mean,
+                                2), data)) / len(data)) ** .5)
