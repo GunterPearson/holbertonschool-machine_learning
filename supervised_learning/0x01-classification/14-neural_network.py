6f79b0b3a9cc46aae727c7899968a1213ac4cbb9
GunterPearson <1715@holbertonschool.com>
2020-12-30 14:07:04 -0600

    pushing updated 14-NN

--- supervised_learning/0x01-classification/14-neural_network.py
+++ supervised_learning/0x01-classification/14-neural_network.py
@@ -88,11 +88,11 @@ class NeuralNetwork:
 
     def train(self, X, Y, iterations=5000, alpha=0.05):
         """ train model based on gradient descent"""
-        if type(iterations) != int:
+        if type(iterations) is not int:
             raise TypeError("iterations must be an integer")
         if iterations <= 0:
             raise ValueError("iterations must be a positive integer")
-        if type(alpha) != float:
+        if type(alpha) is not float:
             raise TypeError("alpha must be a float")
         if alpha <= 0:
             raise ValueError("alpha must be positive")
