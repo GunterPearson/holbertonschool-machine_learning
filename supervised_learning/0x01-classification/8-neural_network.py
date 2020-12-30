71906c3cea2e9b90b11784a66823dac0243d4735
GunterPearson <1715@holbertonschool.com>
2020-12-30 12:26:34 -0600

    pushing file 8-NN

---
+++ supervised_learning/0x01-classification/8-neural_network.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+""" neural network class"""
+import numpy as np
+
+
+class NeuralNetwork:
+    """ Creating Neural Network class"""
+
+    def __init__(self, nx, nodes):
+        """ initialize class"""
+        if type(nx) != int:
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        if type(nodes) != int:
+            raise TypeError("nodes must be an integer")
+        if nodes < 1:
+            raise ValueError("nodes must be a positive integer")
+        self.W1 = np.random.normal(size=(nodes, nx))
+        self.b1 = np.zeros((nodes, 1))
+        self.A1 = 0
+        self.W2 = np.random.normal(size=(1, nodes))
+        self.b2 = 0
+        self.A2 = 0
