190c1884631d83db98f815a2a533b364608588e6
GunterPearson <1715@holbertonschool.com>
2021-01-04 14:39:00 -0600

    pushing file 16-deepNN

---
+++ supervised_learning/0x01-classification/16-deep_neural_network.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+""" Deep Neural Network"""
+import numpy as np
+
+
+class DeepNeuralNetwork:
+    """ creating deepNN class"""
+
+    def __init__(self, nx, layers):
+        """ initialize deepNN"""
+        if type(nx) != int:
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        if type(layers) != list or layers == []:
+            raise TypeError("layers must be a list of positive integers")
+        check = all(x > 0 for x in layers)
+        if not check:
+            raise TypeError("layers must be a list of positive integers")
+        self.L = len(layers)
+        self.cache = {}
+        self.weights = {"W1": np.random.randn(layers[0],
+                        nx) * np.sqrt(2 / nx),
+                        "b1": np.zeros((layers[0], 1))}
+        for x in range(1, self.L):
+            W = "W" + str(x + 1)
+            B = "b" + str(x + 1)
+            self.weights[W] = np.random.randn(
+                              layers[x],
+                              layers[x - 1]) * np.sqrt(2 / layers[x - 1])
+            self.weights[B] = np.zeros((layers[x], 1))
