0a038e27238019c95f9900a4d6eebd700d1d2a97
GunterPearson <1715@holbertonschool.com>
2021-01-05 16:17:02 -0600

    pushing file 25-one_hot_encode

--- supervised_learning/0x01-classification/25-one_hot_decode.py
+++ supervised_learning/0x01-classification/25-one_hot_decode.py
@@ -5,6 +5,8 @@ import numpy as np
 
 def one_hot_decode(one_hot):
     """ decode hot"""
+    if type(one_hot) is not np.ndarray:
+        return None
     try:
         return np.argmax(one_hot.T, axis=1)
     except Exception:
