b5d72c3ae0f9023556cc000a1fe5726c1c57935b
GunterPearson <1715@holbertonschool.com>
2021-01-05 16:07:47 -0600

    pushing file 24-one

--- supervised_learning/0x01-classification/24-one_hot_encode.py
+++ supervised_learning/0x01-classification/24-one_hot_encode.py
@@ -5,6 +5,8 @@ import numpy as np
 
 def one_hot_encode(Y, classes):
     """ one hot encode"""
+    if type(Y) is not np.ndarray:
+        return None
     try:
         x = np.eye(classes)[Y].T
         return x
