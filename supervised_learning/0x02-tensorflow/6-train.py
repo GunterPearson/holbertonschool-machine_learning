f51a88eaf646598e8fdbf19528c6b8bd8cba30b5
GunterPearson <1715@holbertonschool.com>
2021-01-11 18:36:05 -0600

    pushing file 6-train

---
+++ supervised_learning/0x02-tensorflow/6-train.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+""" train"""
+import tensorflow as tf
+calculate_accuracy = __import__('3-calculate_accuracy').calculate_accuracy
+calculate_loss = __import__('4-calculate_loss').calculate_loss
+create_placeholders = __import__('0-create_placeholders').create_placeholders
+create_train_op = __import__('5-create_train_op').create_train_op
+forward_prop = __import__('2-forward_prop').forward_prop
+
+
+def train(X_train, Y_train, X_valid, Y_valid, layer_sizes,
+          activations, alpha, iterations, save_path="/tmp/model.ckpt"):
+    """ train model"""
+    x, y = create_placeholders(X_train.shape[1], Y_train.shape[1])
+    y_pred = forward_prop(x, layer_sizes, activations)
+    accuracy = calculate_accuracy(y, y_pred)
+    loss = calculate_loss(y, y_pred)
+    train_op = create_train_op(loss, alpha)
+    init = tf.global_variables_initializer()
+    saver = tf.train.Saver()
+    with tf.Session() as sess:
+        sess.run(init)
+        for i in range(iterations + 1):
+            cost_t = sess.run(loss,
+                              feed_dict={x: X_train, y: Y_train})
+            acc_t = sess.run(accuracy,
+                             feed_dict={x: X_train, y: Y_train})
+            cost_v = sess.run(loss,
+                              feed_dict={x: X_valid, y: Y_valid})
+            acc_v = sess.run(accuracy,
+                             feed_dict={x: X_valid, y: Y_valid})
+            if i % 100 == 0 or i == iterations:
+                print("After {} iterations:".format(i))
+                print("\tTraining Cost: {}".format(cost_t))
+                print("\tTraining Accuracy: {}".format(acc_t))
+                print("\tValidation Cost: {}".format(cost_v))
+                print("\tValidation Accuracy: {}".format(acc_v))
+            if i < iterations:
+                sess.run(train_op, feed_dict={x: X_train, y: Y_train})
+        return saver.save(sess, save_path)
