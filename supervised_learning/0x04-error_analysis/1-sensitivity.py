44eabf42949bf3a1d394a52a7e6646e7c6c3d81b
GunterPearson <1715@holbertonschool.com>
2021-01-20 10:56:51 -0600

    pushing file 1-senitivity.py

--- supervised_learning/0x04-error_analysis/1-sensitivity.py
+++ supervised_learning/0x04-error_analysis/1-sensitivity.py
@@ -6,5 +6,5 @@ import numpy as np
 def sensitivity(confusion):
     """ calculate sensitivity"""
     ay = np.sum(confusion, axis=1)
-    tp = np.max(confusion, axis=1)
+    tp = np.diagonal(confusion)
     return tp / ay
