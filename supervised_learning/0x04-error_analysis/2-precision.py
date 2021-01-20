a233be4fa7f081820536f1036a812696f6f74226
GunterPearson <1715@holbertonschool.com>
2021-01-20 10:57:59 -0600

    pushing file 2-precision

--- supervised_learning/0x04-error_analysis/2-precision.py
+++ supervised_learning/0x04-error_analysis/2-precision.py
@@ -5,6 +5,6 @@ import numpy as np
 
 def precision(confusion):
     """ calculate precision on confusion matrix"""
-    x = np.sum(confusion, axis=0)
-    y = np.max(confusion, axis=1)
-    return y / x
+    py = np.sum(confusion, axis=0)
+    tp = np.diagonal(confusion)
+    return tp / py
