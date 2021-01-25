0e7ce96a69c0c6158e710f75f6301e0b4792920f
GunterPearson <1715@holbertonschool.com>
2021-01-25 14:02:56 -0600

    pushing file 1-l2_reg_gradient_descent

--- supervised_learning/0x05-regularization/1-l2_reg_gradient_descent.py
+++ supervised_learning/0x05-regularization/1-l2_reg_gradient_descent.py
@@ -6,16 +6,13 @@ import numpy as np
 def l2_reg_gradient_descent(Y, weights, cache, alpha, lambtha, L):
     """ gradient descent with l2 norm"""
     m = Y.shape[1]
-    for x in reversed(range(1, L + 1)):
-        AN1 = cache["A" + str(x - 1)]
-        A0 = cache["A" + str(x)]
-        W0 = weights["W" + str(x)]
-        if x == L:
-            dz = A0 - Y
-        else:
-            dz = da * (A0 * (1 - A0))
-        db = dz.mean(axis=1, keepdims=True)
-        dw = np.matmul(dz, AN1.T) / m + ((lambtha / m) * weights['W' + str(x)])
-        da = np.matmul(W0.T, dz)
-        weights['W' + str(x)] -= (alpha * dw)
-        weights['b' + str(x)] -= (alpha * db)
+    dz = cache["A" + str(L)] - Y
+    for x in range(L, 0, -1):
+        A_prev = cache["A" + str(x - 1)]
+        dw = np.matmul(dz, A_prev.T) / m
+        db = np.sum(dz, axis=1, keepdims=True) / m
+        inv = 1 - np.square(A_prev)
+        dz = np.matmul(weights["W" + str(x)].T, dz) * inv
+        reg = (1 - lambtha * alpha / m)
+        weights["W" + str(x)] = reg * weights["W" + str(x)] - alpha * dw
+        weights["b" + str(x)] -= alpha * db
