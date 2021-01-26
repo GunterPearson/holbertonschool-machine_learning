af1808c0836da90a91a399cfc9daf4cd288a5be2
GunterPearson <1715@holbertonschool.com>
2021-01-26 00:23:41 -0600

    pushing file 4-train.py

--- supervised_learning/0x06-keras/4-train.py
+++ supervised_learning/0x06-keras/4-train.py
@@ -6,9 +6,9 @@ import tensorflow.keras as K
 def train_model(network, data, labels, batch_size,
                 epochs, verbose=True, shuffle=False):
     """ train model using mini-batch"""
-    history = network.fit(x=data, y=labels,
-                          epochs=epochs,
+    History = network.fit(x=data, y=labels,
                           batch_size=batch_size,
+                          epochs=epochs,
                           verbose=verbose,
                           shuffle=shuffle)
-    return history
+    return History
