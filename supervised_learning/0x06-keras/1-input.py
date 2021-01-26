e3352d41b8d5285e2c13c90f643a388d8e815f7d
GunterPearson <1715@holbertonschool.com>
2021-01-26 00:17:01 -0600

    pushing pycodestyle file 1-input.py

--- supervised_learning/0x06-keras/1-input.py
+++ supervised_learning/0x06-keras/1-input.py
@@ -8,12 +8,12 @@ def build_model(nx, layers, activations, lambtha, keep_prob):
     inputs = K.Input(shape=(nx,))
     reg = K.regularizers.l2(lambtha)
     new = K.layers.Dense(layers[0],
-                             activation=activations[0],
-                             kernel_regularizer=reg)(inputs)
+                         activation=activations[0],
+                         kernel_regularizer=reg)(inputs)
     for x in range(1, len(layers)):
         new = K.layers.Dropout(1 - keep_prob)(new)
         new = K.layers.Dense(layers[x],
-                                 activation=activations[x],
-                                 kernel_regularizer=reg)(new)
+                             activation=activations[x],
+                             kernel_regularizer=reg)(new)
     model = K.Model(inputs=inputs, outputs=new)
     return model
