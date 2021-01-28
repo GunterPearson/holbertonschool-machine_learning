741c77493dcfcc6bb0e3f5f89d2be6cff54df7da
GunterPearson <1715@holbertonschool.com>
2021-01-28 13:04:47 -0600

    pushing file 0-convolve_grayscale_valid

--- math/0x04-convolutions_and_pooling/0-convolve_grayscale_valid.py
+++ math/0x04-convolutions_and_pooling/0-convolve_grayscale_valid.py
@@ -10,8 +10,8 @@ def convolve_grayscale_valid(images, kernel):
     c_h = h - kh + 1
     c_w = w - kw + 1
     conv = np.zeros((m, c_h, c_h))
-    for h in range(c_h):
-        for w in range(c_w):
+    for w in range(c_h):
+        for h in range(c_w):
             square = images[:, h: h + kh, w: w + kw]
             insert = np.sum(kernel * square, axis=(1, 2))
             conv[:, h, w] = insert
