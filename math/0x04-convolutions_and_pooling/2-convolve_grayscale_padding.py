536e386c536f40831bff5e5695a2e39c90e78332
GunterPearson <1715@holbertonschool.com>
2021-01-28 14:40:12 -0600

    pushing file 2-convolve_grayscal_padding.py

---
+++ math/0x04-convolutions_and_pooling/2-convolve_grayscale_padding.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+""" convolutions"""
+import numpy as np
+
+
+def convolve_grayscale_padding(images, kernel, padding):
+    """ convolve with custom padding"""
+    kh, kw = kernel.shape
+    m, hm, wm = images.shape
+    ph, pw = padding
+    padded = np.pad(images, ((0, 0), (ph, ph), (pw, pw)), 'constant')
+    ch = hm + (2 * ph) - kh + 1
+    cw = wm + (2 * pw) - kw + 1
+    convoluted = np.zeros((m, ch, cw))
+    for h in range(hm):
+        for w in range(wm):
+            square = padded[:, h: h + kh, w: w + kw]
+            insert = np.sum(square * kernel, axis=1).sum(axis=1)
+            convoluted[:, h, w] = insert
+    return convoluted
