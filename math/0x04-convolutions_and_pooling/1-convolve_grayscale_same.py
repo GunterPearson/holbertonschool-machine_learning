aabc8cf964fc46eebbff7acc8d6b7ee67cc28adc
GunterPearson <1715@holbertonschool.com>
2021-01-28 14:23:27 -0600

    pushing file 1-convolve_grayscale_same.py

--- math/0x04-convolutions_and_pooling/1-convolve_grayscale_same.py
+++ math/0x04-convolutions_and_pooling/1-convolve_grayscale_same.py
@@ -7,7 +7,7 @@ def convolve_grayscale_same(images, kernel):
     """ convolve grayscale same"""
     hk, wk = kernel.shape
     m, hm, wm = images.shape
-    P = int((hk - 1) / 2)
+    P = int(hk / 2)
     padded = np.pad(images, ((0, 0), (P, P), (P, P)), 'constant')
     convoluted = np.zeros((m, hm, wm))
     for h in range(hm):
