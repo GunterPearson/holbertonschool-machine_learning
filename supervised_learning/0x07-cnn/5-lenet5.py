adbf1806c69d0ba32022e3cf950dd93e30364b3e
GunterPearson <1715@holbertonschool.com>
2021-02-03 12:59:30 -0600

    pushing updated 5-lenet5.py

--- supervised_learning/0x07-cnn/5-lenet5.py
+++ supervised_learning/0x07-cnn/5-lenet5.py
@@ -5,24 +5,26 @@ import tensorflow.keras as K
 
 def lenet5(X):
     """ lenet with keras"""
-    conv1 = K.layers.Conv2D(filters=6,
-                            kernel_size=(5, 5),
-                            activation='relu', padding="same",
-                            kernel_initializer="he_normal")(X)
-    pool1 = K.layers.MaxPool2D(pool_size=(2, 2), strides=(2, 2))(conv1)
-    conv2 = K.layers.Conv2D(filters=16, kernel_size=(5, 5),
-                            activation='relu', padding="valid",
-                            kernel_initializer="he_normal")(pool1)
-    pool2 = K.layers.MaxPool2D(pool_size=(2, 2), strides=(2, 2))(conv2)
-    pool2_flat = K.layers.Flatten()(pool2)
-    FL1 = K.layers.Dense(units=120, activation='relu',
-                         kernel_initializer="he_normal")(pool2_flat)
-    FL2 = K.layers.Dense(units=84, activation='relu',
-                         kernel_initializer="he_normal")(FL1)
-    FL3 = K.layers.Dense(units=10, activation='softmax',
-                         kernel_initializer="he_normal")(FL2)
-    opt = K.optimizers.Adam()
-    model = K.Model(inputs=X, outputs=FL3)
-    model.compile(loss='categorical_crossentropy',
-                  optimizer=opt, metrics=["accuracy"])
+    model = K.Sequential()
+    L1 = K.layers.Conv2D(filters=6, kernel_size=(5, 5),
+                         activation='relu', padding="same",
+                         kernel_initializer="he_normal")(X)
+    p1 = K.layers.MaxPool2D(pool_size=(2, 2), strides=(2, 2))(L1)
+    L2 = K.layers.Conv2D(filters=16, kernel_size=(5, 5),
+                         activation='relu', padding="valid",
+                         kernel_initializer="he_normal")(p1)
+    p2 = K.layers.MaxPool2D(pool_size=(2, 2), strides=(2, 2))(L2)
+    flat = K.layers.Flatten()(p2)
+    L3 = K.layers.Dense(units=120,
+                        activation='relu',
+                        kernel_initializer="he_normal")(flat)
+    L4 = K.layers.Dense(units=84,
+                        activation='relu',
+                        kernel_initializer="he_normal")(L3)
+    y_pred = K.layers.Dense(units=10,
+                            activation='softmax',
+                            kernel_initializer="he_normal")(L4)
+    model = K.Model(inputs=X, outputs=y_pred)
+    model.compile(optimizer=K.optimizers.Adam(),
+                  loss='categorical_crossentropy', metrics=['accuracy'])
     return model
