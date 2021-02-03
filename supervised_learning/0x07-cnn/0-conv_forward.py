652a029e7ce1b4f5a4eef1da58beadbac2ed2dd8
GunterPearson <1715@holbertonschool.com>
2021-02-03 12:09:13 -0600

    pushing file 0-conv_forward.py

--- supervised_learning/0x07-cnn/0-conv_forward.py
+++ supervised_learning/0x07-cnn/0-conv_forward.py
@@ -8,12 +8,12 @@ def conv_forward(A_prev, W, b, activation, padding="same", stride=(1, 1)):
     m, h_prev, w_prev, c_prev = A_prev.shape
     kh, kw, c_prev, c_new = W.shape
     sh, sw = stride
-    if padding == 'same':
-        ph = int(((h_prev - 1) * sh + kh - h_prev) / 2) + 1
-        pw = int(((w_prev - 1) * sw + kw - w_prev) / 2) + 1
-    elif padding == 'valid':
+    if padding == 'valid':
         ph = 0
         pw = 0
+    else:
+        ph = int(((h_prev - 1) * sh + kh - h_prev) / 2) + 1
+        pw = int(((w_prev - 1) * sw + kw - w_prev) / 2) + 1
     out_h = int((h_prev + 2 * ph - kh) / sh) + 1
     out_w = int((w_prev + 2 * pw - kw) / sw) + 1
     padded = np.pad(A_prev, ((0, 0), (ph, ph), (pw, pw), (0, 0)), 'constant')
@@ -21,7 +21,7 @@ def conv_forward(A_prev, W, b, activation, padding="same", stride=(1, 1)):
     for c in range(c_new):
         for h in range(out_h):
             for w in range(out_w):
-                square = padded[:, h * sh: h * sh + kh, w * sw: w * sw + kw, :]
+                square = padded[:, h * sh: h * sh + kh, w * sw: w * sw + kw]
                 insert = np.sum(square * W[..., c], axis=(1, 2, 3))
                 conv[:, h, w, c] = insert
     result = conv + b
