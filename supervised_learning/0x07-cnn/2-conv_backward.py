8efa25a39a9bf569f0159862c1d5936df6f6f7c0
GunterPearson <1715@holbertonschool.com>
2021-02-03 12:47:12 -0600

    pushing updated 2-conv_backward.py

--- supervised_learning/0x07-cnn/2-conv_backward.py
+++ supervised_learning/0x07-cnn/2-conv_backward.py
@@ -5,36 +5,37 @@ import numpy as np
 
 def conv_backward(dZ, A_prev, W, b, padding="same", stride=(1, 1)):
     """ conv back"""
+    hk = W.shape[0]
+    wk = W.shape[1]
+    nc = W.shape[3]
     m = A_prev.shape[0]
-    h_prev = A_prev.shape[1]
-    w_prev = A_prev.shape[2]
-    c_prev = A_prev.shape[3]
-    kh = W.shape[0]
-    kw = W.shape[1]
-    h_new = dZ.shape[1]
-    w_new = dZ.shape[2]
-    c_new = dZ.shape[3]
-    sh = stride[0]
-    sw = stride[1]
-    if padding == 'same':
-        ph = int(((h_prev - 1) * sh + kh - h_prev) / 2) + 1
-        pw = int(((w_prev - 1) * sw + kw - w_prev) / 2) + 1
-    elif padding == 'valid':
-        ph = 0
-        pw = 0
-    padded = np.pad(A_prev, ((0, 0), (ph, ph), (pw, pw), (0, 0)), 'constant')
-    da = np.zeros(A_prev.shape)
-    dw = np.zeros(W.shape)
+    hm = A_prev.shape[1]
+    wm = A_prev.shape[2]
+    st1 = stride[1]
+    st0 = stride[0]
+    if padding == "valid":
+        pad0 = 0
+        pad1 = 0
+    else:
+        pad0 = int(((hm - 1) * st0 + hk - hm) / 2) + 1
+        pad1 = int(((wm - 1) * st1 + wk - wm) / 2) + 1
+    out_h = int((hm + 2 * pad0 - hk) / st0) + 1
+    out_w = int((wm + 2 * pad1 - wk) / st1) + 1
+    x_pad = np.pad(A_prev, ((0, 0), (pad0, pad0), (
+                           pad1, pad1), (0, 0)), 'constant')
+    dX = np.zeros(x_pad.shape)
+    dW = np.zeros(W.shape)
     db = np.sum(dZ, axis=(0, 1, 2), keepdims=True)
-    for n in range(m):
-        for h in range(h_new):
-            for w in range(w_new):
-                for c in range(c_new):
-                    da[n, h * sh: h * sh + kh, w * sw: w * sw + kw,
-                       :] += dZ[n, h, w, c] * W[..., c]
-                    dw[..., c] += padded[n, h * sh: h * sh +
-                                         kh, w * sw: w * sw + kw,
-                                         :] * dZ[n, h, w, c]
-    if padding == 'same':
-        da = da[:, ph:-ph, pw:-pw, :]
-    return da, dw, db
+    for i in range(m):
+        for h in range(out_h):
+            for w in range(out_w):
+                for c in range(nc):
+                    dX[i, h * st0: h * st0 + hk,
+                       w * st1: w * st1 + wk, :] += dZ[
+                       i, h, w, c] * W[:, :, :, c]
+                    dW[:, :, :, c] += x_pad[i, h * st0: h * st0 + hk,
+                                            w * st1: w * st1 + wk,
+                                            :] * dZ[i, h, w, c]
+    if padding == "same":
+        dX = dX[:, pad0:-pad0, pad1:-pad1, :]
+    return(dX, dW, db)
