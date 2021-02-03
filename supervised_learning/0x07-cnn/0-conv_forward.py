274e2279237a76581663efae9d1ccf4bc7816ce3
GunterPearson <1715@holbertonschool.com>
2021-02-03 12:41:31 -0600

    pushing updated 0-conv

--- supervised_learning/0x07-cnn/0-conv_forward.py
+++ supervised_learning/0x07-cnn/0-conv_forward.py
@@ -5,24 +5,30 @@ import numpy as np
 
 def conv_forward(A_prev, W, b, activation, padding="same", stride=(1, 1)):
     """ conv forward"""
-    m, h_prev, w_prev, _ = A_prev.shape
-    kh, kw, _, c_new = W.shape
-    sh, sw = stride
-    if padding == 'valid':
-        ph = 0
-        pw = 0
+    hk = W.shape[0]
+    wk = W.shape[1]
+    nc = W.shape[3]
+    m = A_prev.shape[0]
+    hm = A_prev.shape[1]
+    wm = A_prev.shape[2]
+    st1 = stride[1]
+    st0 = stride[0]
+    if padding == "valid":
+        pad0 = 0
+        pad1 = 0
     else:
-        ph = int(((h_prev - 1) * sh + kh - h_prev) / 2) + 1
-        pw = int(((w_prev - 1) * sw + kw - w_prev) / 2) + 1
-    out_h = int((h_prev + 2 * ph - kh) / sh) + 1
-    out_w = int((w_prev + 2 * pw - kw) / sw) + 1
-    padded = np.pad(A_prev, ((0, 0), (ph, ph), (pw, pw), (0, 0)), 'constant')
-    conv = np.zeros((m, out_h, out_w, c_new))
-    for h in range(out_h):
-        for w in range(out_w):
-            for c in range(c_new):
-                square = padded[:, h * sh: h * sh + kh, w * sw: w * sw + kw]
-                insert = np.sum(square * W[..., c], axis=(1, 2, 3))
-                conv[:, h, w, c] = insert
-    result = conv + b
-    return activation(result)
+        pad0 = int(((hm - 1) * st0 + hk - hm) / 2)
+        pad1 = int(((wm - 1) * st1 + wk - wm) / 2)
+    out_h = int((hm + 2 * pad0 - hk) / st0) + 1
+    out_w = int((wm + 2 * pad1 - wk) / st1) + 1
+    convoluted = np.zeros((m, out_h, out_w, nc))
+    img = np.pad(A_prev, ((0, 0), (pad0, pad0), (
+                           pad1, pad1), (0, 0)), 'constant')
+    for c in range(nc):
+        for h in range(out_h):
+            for w in range(out_w):
+                ma = img[:, h * st0: h * st0 + hk, w * st1: w * st1 + wk]
+                vm = np.sum(ma * W[..., c], axis=(1, 2, 3))
+                convoluted[:, h, w, c] = vm
+    z = convoluted + b
+    return(activation(z))
