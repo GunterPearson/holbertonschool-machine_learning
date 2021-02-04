7036c59b29ade01d99ac5f3257c9e63fc6dda84a
GunterPearson <1715@holbertonschool.com>
2021-02-04 13:11:32 -0600

    pushing file 5-lenet5.py

--- supervised_learning/0x07-cnn/5-lenet5.py
+++ supervised_learning/0x07-cnn/5-lenet5.py
@@ -5,19 +5,24 @@ import tensorflow.keras as K
 
 def lenet5(X):
     """ lenet with keras"""
-    L1 = K.layers.Conv2D(filters=6, kernel_size=(5, 5),
+    init = K.initializers.he_normal(seed=None)
+    L1 = K.layers.Conv2D(filters=6, kernel_size=5,
                          activation='relu', padding="same",
-                         kernel_initializer="he_normal")(X)
-    p1 = K.layers.MaxPool2D(pool_size=(2, 2), strides=(2, 2))(L1)
-    L2 = K.layers.Conv2D(filters=16, kernel_size=(5, 5),
+                         kernel_initializer=init)(X)
+    p1 = K.layers.MaxPool2D(pool_size=2, strides=(2, 2))(L1)
+    L2 = K.layers.Conv2D(filters=16, kernel_size=5,
                          activation='relu', padding="valid",
-                         kernel_initializer="he_normal")(p1)
-    p2 = K.layers.MaxPool2D(pool_size=(2, 2), strides=(2, 2))(L2)
+                         kernel_initializer=init)(p1)
+    p2 = K.layers.MaxPool2D(pool_size=2, strides=(2, 2))(L2)
     flat = K.layers.Flatten()(p2)
-    L3 = K.layers.Dense(units=120, activation='relu')(flat)
-    L4 = K.layers.Dense(units=84, activation='relu')(L3)
-    y_pred = K.layers.Dense(units=10, activation='softmax')(L4)
+    L3 = K.layers.Dense(units=120, activation='relu',
+                        kernel_initializer=init)(flat)
+    L4 = K.layers.Dense(units=84, activation='relu',
+                        kernel_initializer=init)(L3)
+    y_pred = K.layers.Dense(units=10, activation='softmax',
+                            kernel_initializer=init)(L4)
     model = K.Model(inputs=X, outputs=y_pred)
-    model.compile(optimizer=K.optimizers.Adam(),
-                  loss='categorical_crossentropy', metrics=['accuracy'])
+    adam = K.optimizers.Adam()
+    model.compile(optimizer=adam, loss='categorical_crossentropy',
+                  metrics=['accuracy'])
     return model
