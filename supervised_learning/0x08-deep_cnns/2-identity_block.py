a16e9cc04e026eb41a2c52ebb3b3771035f7bbec
GunterPearson <1715@holbertonschool.com>
2021-02-10 07:39:49 -0600

    pushing file 2-identity_block.py

---
+++ supervised_learning/0x08-deep_cnns/2-identity_block.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+""" inception"""
+import tensorflow.keras as K
+
+
+def identity_block(A_prev, filters):
+    """ Identity_block"""
+    F11, F3, F12 = filters
+    conv1 = K.layers.Conv2D(filters=F11,
+                            strides=1,
+                            kernel_size=(1, 1),
+                            padding='same')(A_prev)
+    batch1 = K.layers.BatchNormalization(axis=3)(conv1)
+    act1 = K.layers.Activation('relu')(batch1)
+    conv2 = K.layers.Conv2D(filters=F3,
+                            strides=1,
+                            kernel_size=(3, 3),
+                            padding='same')(act1)
+    batch2 = K.layers.BatchNormalization(axis=3)(conv2)
+    act2 = K.layers.Activation('relu')(batch2)
+    conv3 = K.layers.Conv2D(filters=F12,
+                            strides=1,
+                            kernel_size=(1, 1),
+                            padding='same')(act2)
+    batch3 = K.layers.BatchNormalization(axis=3)(conv3)
+    add1 = K.layers.Add()([batch3, A_prev])
+    act3 = K.layers.Activation('relu')(add1)
+    return act3
