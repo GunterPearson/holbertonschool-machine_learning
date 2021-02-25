a6e2721144841d1ca336455fcf59e1be26e18ccf
GunterPearson <1715@holbertonschool.com>
2021-02-25 13:06:55 -0600

    pushing file 0-yolo.py

---
+++ supervised_learning/0x0A-object_detection/0-yolo.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+""" yolo class"""
+import tensorflow.keras as K
+
+
+class Yolo():
+    """ yolo class"""
+    def __init__(self, model_path, classes_path, class_t, nms_t, anchors):
+        """ class constructor"""
+        self.model = K.models.load_model(model_path)
+        with open(classes_path, 'r') as f:
+            line = [s.rstrip('\n') for s in f]
+        self.class_names = line
+        self.class_t = class_t
+        self.nms_t = nms_t
+        self.anchors = anchors
