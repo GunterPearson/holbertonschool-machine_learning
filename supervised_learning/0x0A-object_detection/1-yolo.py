6851ae188ddbb8707dc43651992a5ed5deb21352
GunterPearson <1715@holbertonschool.com>
2021-02-26 04:34:31 -0600

    pushing file 1-yolo.py

--- supervised_learning/0x0A-object_detection/1-yolo.py
+++ supervised_learning/0x0A-object_detection/1-yolo.py
@@ -26,26 +26,8 @@ class Yolo():
         boxes = []
         box_conf = []
         box_class = []
-        boxes = [output[:, :, :, 0:4] for output in outputs]
-        for oidx, output in enumerate(boxes):
-            for y in range(output.shape[0]):
-                for x in range(output.shape[1]):
-                    c_y = ((self.sigmoid(output[y, x, :, 1]) + y)
-                           / output.shape[0] * image_size[0])
-                    c_x = ((self.sigmoid(output[y, x, :, 0]) + x)
-                           / output.shape[1] * image_size[1])
-                    resize = self.anchors[oidx].astype(float)
-                    resize[:, 0] *= (np.exp(output[y, x, :, 2])
-                                     / 2 * image_size[1] /
-                                     self.model.input.shape[1].value)
-                    resize[:, 1] *= (np.exp(output[y, x, :, 3])
-                                     / 2 * image_size[0] /
-                                     self.model.input.shape[2].value)
-                    output[y, x, :, 0] = c_x - resize[:, 0]
-                    output[y, x, :, 1] = c_y - resize[:, 1]
-                    output[y, x, :, 2] = c_x + resize[:, 0]
-                    output[y, x, :, 3] = c_y + resize[:, 1]
-        for output in outputs:
-            box_conf.append(self.sigmoid(output[..., 4, np.newaxis]))
-            box_class.append(self.sigmoid(output[..., 5:]))
+        for out in outputs:
+            boxes.append(out[..., 0:4])
+            box_conf.append(self.sigmoid(out[..., 4, np.newaxis]))
+            box_class.append(self.sigmoid(out[..., 5:]))
         return boxes, box_conf, box_class
