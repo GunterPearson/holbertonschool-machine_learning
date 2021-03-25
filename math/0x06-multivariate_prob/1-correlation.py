41dcb6a05c91b7d13f4e4530ab367daa3fd3966f
GunterPearson <1715@holbertonschool.com>
2021-03-24 19:04:41 -0500

    pushing file 1-correlation.py

--- math/0x06-multivariate_prob/1-correlation.py
+++ math/0x06-multivariate_prob/1-correlation.py
@@ -6,9 +6,9 @@ import numpy as np
 def correlation(C):
     """ correlation"""
     if not isinstance(C, np.ndarray):
-        raise TypeError('C must be a 2D numpy.ndarray')
-    if C.shape[0] < 2:
-        raise ValueError("C must contain multiple data points")
+        raise TypeError('C must be a numpy.ndarray')
+    if len(C.shape) != 2 or C.shape[0] != C.shape[1]:
+        raise ValueError('C must be a 2D square matrix')
 
     x = np.diag(1 / np.sqrt(np.diag(C)))
     cor = x @ C @ x
