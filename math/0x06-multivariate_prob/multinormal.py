df6dc9c41dc3a5838cd169f447956cf625b37e9b
GunterPearson <1715@holbertonschool.com>
2021-03-24 20:04:12 -0500

    pushing file multinormal.py for checking

--- math/0x06-multivariate_prob/multinormal.py
+++ math/0x06-multivariate_prob/multinormal.py
@@ -30,11 +30,7 @@ class MultiNormal():
         if len(x.shape) != 2 or x.shape != (d, 1):
             raise ValueError('x must have the shape ({}, 1)'.format(d))
 
-        n = x.shape[0]
-        m = self.mean
-        c = self.cov
-        t = np.sqrt(((2 * np.pi) ** n) * np.linalg.det(c))
-        j = np.linalg.inv(c)
-        ran = (-0.5 * np.matmul(np.matmul((x - m).T, j), x - self.mean))
-        pdf = (1 / t) * np.exp(ran[0][0])
-        return pdf
+        res = np.matmul((x - self.mean).T, np.linalg.inv(self.cov))
+        res = np.exp(np.matmul(res, (x - self.mean)) / -2)
+        res /= np.sqrt(pow(2 * np.pi, x.shape[0]) * np.linalg.det(self.cov))
+        return res[0][0]
