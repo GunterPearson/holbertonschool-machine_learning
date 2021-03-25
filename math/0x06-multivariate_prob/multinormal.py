65ee15a2307aa3d43c6670f88f0a690988eecb49
GunterPearson <1715@holbertonschool.com>
2021-03-24 19:54:04 -0500

    pushing file multinormal.py for checking

--- math/0x06-multivariate_prob/multinormal.py
+++ math/0x06-multivariate_prob/multinormal.py
@@ -21,3 +21,16 @@ class MultiNormal():
         mean = np.expand_dims(np.mean(X, axis=1), axis=1)
         cov = np.matmul((X - mean), (X - mean).T) / (n - 1)
         return mean, cov
+
+    def pdf(self, x):
+        """ return pdf"""
+        if not isinstance(x, np.ndarray):
+            raise TypeError('x must be a numpy.ndarray')
+        d = self.cov.shape[0]
+        if len(x.shape) != 2 or x.shape != (d, 1):
+            raise ValueError('x must have the shape ({}, 1)'.format(d))
+
+        res = np.matmul((x - self.mean).T, np.linalg.inv(self.cov))
+        res = np.exp(np.matmul(res, (x - self.mean)) / -2)
+        res /= np.sqrt(pow(2 * np.pi, x.shape[0]) * np.linalg.det(self.cov))
+        return res[0][0]
