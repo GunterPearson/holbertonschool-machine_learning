a9e4f3ea033ec4da9f451712f1528451d2d3fc1d
GunterPearson <1715@holbertonschool.com>
2021-03-24 19:20:43 -0500

    pushing file multinormal.py

---
+++ math/0x06-multivariate_prob/multinormal.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""multivaritive cov"""
+import numpy as np
+
+
+class MultiNormal():
+    """ mulit normal class"""
+    def __init__(self, data):
+        """ initializer"""
+        if not isinstance(data, np.ndarray) or len(data.shape) != 2:
+            raise TypeError('data must be a 2D numpy.ndarray')
+        if data.shape[0] < 2:
+            raise ValueError("data must contain multiple data points")
+        self.mean, self.cov = self.mean_cov(data)
+
+    @staticmethod
+    def mean_cov(X):
+        """ caclulate mean covariance"""
+        d, n = X.shape
+        m = np.mean(X, axis=0, keepdims=True)
+        cov = np.matmul((X - m).T, X - m) / (n - 1)
+        return m, cov
