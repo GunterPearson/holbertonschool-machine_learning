c64f486cae5a33bf7a27ac112d4e41104a943329
GunterPearson <1715@holbertonschool.com>
2021-03-28 22:19:57 -0500

    pushing updated files

--- math/0x07-bayesian_prob/2-marginal.py
+++ math/0x07-bayesian_prob/2-marginal.py
@@ -5,16 +5,16 @@ import numpy as np
 
 def likelihood(x, n, P):
     """ likelihood"""
-    if n < 1:
+    if type(n) is not int or n <= 0:
         raise ValueError("n must be a positive integer")
-    if x < 0:
-        y = "x must be an integer that is greater than or equal to 0"
-        raise ValueError(y)
+    if type(x) is not int or x < 0:
+        raise ValueError("x must be an integer that "
+                         "is greater than or equal to 0")
     if x > n:
         raise ValueError("x cannot be greater than n")
-    if not isinstance(P, np.ndarray) or len(P.shape) != 1:
+    if type(P) is not np.ndarray or len(P.shape) != 1:
         raise TypeError("P must be a 1D numpy.ndarray")
-    if any(j >= 1 and j <= 0 for j in P):
+    if np.where(P > 1, 1, 0).any() or np.where(P < 0, 1, 0).any():
         raise ValueError("All values in P must be in the range [0, 1]")
 
     new = np.math.factorial(n)
