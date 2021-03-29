faee58f32a91fbf5eaab177c5c5e89c230519365
GunterPearson <1715@holbertonschool.com>
2021-03-28 22:23:56 -0500

    pushing updated files

--- math/0x07-bayesian_prob/2-marginal.py
+++ math/0x07-bayesian_prob/2-marginal.py
@@ -46,11 +46,22 @@ def intersection(x, n, P, Pr):
 def marginal(x, n, P, Pr):
     """ marginal"""
     like = likelihood(x, n, P)
-    if np.where(Pr > 1, 1, 0).any() or np.where(Pr < 0, 1, 0).any():
-        raise ValueError("All values in Pr must be in the range [0, 1]")
-    if type(Pr) is not np.ndarray or P.shape != Pr.shape:
-        raise TypeError("Pr must be a numpy.ndarray with the same shape as P")
-    if not np.isclose(Pr.sum(), 1):
-        raise ValueError("Pr must sum to 1")
+    if not isinstance(n, int) or n < 1:
+        raise ValueError('n must be a positive integer')
+    if not isinstance(x, int) or x < 0:
+        z = 'x must be an integer that is greater than or equal to 0'
+        raise ValueError(z)
+    if x > n:
+        raise ValueError('x cannot be greater than n')
+    if not isinstance(P, np.ndarray) or len(P.shape) != 1:
+        raise TypeError('P must be a 1D numpy.ndarray')
+    if not isinstance(Pr, np.ndarray) or Pr.shape != P.shape:
+        raise TypeError('Pr must be a numpy.ndarray with the same shape as P')
+    if np.any([h < 0 or h > 1 for h in P]):
+        raise ValueError('All values in P must be in the range [0, 1]')
+    if np.any([h < 0 or h > 1 for h in Pr]):
+        raise ValueError('All values in Pr must be in the range [0, 1]')
+    if not np.isclose(np.sum(Pr), 1):
+        raise ValueError('Pr must sum to 1')
 
     return (like * Pr).sum()
