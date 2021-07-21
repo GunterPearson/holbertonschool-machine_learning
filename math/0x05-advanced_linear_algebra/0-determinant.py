17ba5b7cb4a2fb45acfc36cc22c40ae3adb22b17
GunterPearson <1715@holbertonschool.com>
2021-07-21 16:43:31 -0500

    pushing file 0-determinant

--- math/0x05-advanced_linear_algebra/0-determinant.py
+++ math/0x05-advanced_linear_algebra/0-determinant.py
@@ -5,10 +5,12 @@ import numpy as np
 
 def determinant(matrix):
     """ that calculates the determinant of a matrix """
-    if not all(type(row) == list for row in matrix) or len(matrix) == 0:
+    if not all(type(row) == list for row in matrix):
+        raise TypeError("matrix must be a list of lists")
+    if len(matrix) == 0 or type(matrix) != list:
         raise TypeError("matrix must be a list of lists")
     if matrix == [[]]:
         return 1
-    if len(matrix) != len(matrix[0]):
+    if not all(len(r) == len(matrix) for r in matrix):
         raise ValueError("matrix must be a square matrix")
     return round(np.linalg.det(matrix))
