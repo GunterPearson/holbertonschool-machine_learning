c69f8a76c756d2b11a6c246adb9fe7082cb22ee0
GunterPearson <1715@holbertonschool.com>
2021-07-21 18:25:59 -0500

    pushing file 1-minor.py

---
+++ math/0x05-advanced_linear_algebra/1-minor.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+""" minor """
+
+
+def minor(matrix):
+    """ that calculates the minor of a matrix """
+    if not all(type(row) == list for row in matrix):
+        raise TypeError("matrix must be a list of lists")
+    if len(matrix) == 0 or type(matrix) != list:
+        raise TypeError("matrix must be a list of lists")
+    if len(matrix[0]) == 1:
+        return [[1]]
+    if not all(len(r) == len(matrix) for r in matrix) or matrix == [[]]:
+        raise ValueError("matrix must be a non-empty square matrix")
+    mino = []
+    for x in range(len(matrix)):
+        t = []
+        for y in range(len(matrix[0])):
+            s = []
+            for row in (matrix[:x] + matrix[x + 1:]):
+                s.append(row[:y] + row[y + 1:])
+            t.append(determinant(s))
+        mino.append(t)
+    return mino
+
+
+def determinant(matrix):
+    """ that calculates the determinant of a matrix """
+    if len(matrix) == 1:
+        return matrix[0][0]
+    if len(matrix) == 2:
+        x = (matrix[0][0] * matrix[1][1]) - (matrix[0][1] * matrix[1][0])
+        return x
+    det = 0
+    for x, num in enumerate(matrix):
+        temp = []
+        for row in matrix[1:]:
+            new = []
+            for j in range(len(matrix)):
+                if j != x:
+                    new.append(row[j])
+            temp.append(new)
+        det += num * determinant(temp) * (-1) ** x
+    return det
