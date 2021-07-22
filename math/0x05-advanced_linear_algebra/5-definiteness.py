a1a607126348d487edbda472c149f1893a7edb3f
GunterPearson <1715@holbertonschool.com>
2021-07-21 19:29:40 -0500

    pushing file 5-definiteness.py

---
+++ math/0x05-advanced_linear_algebra/5-definiteness.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+""" advanced linear algebra"""
+import numpy as np
+
+
+def definiteness(matrix):
+    """ definitness"""
+    if not isinstance(matrix, np.ndarray):
+        raise TypeError("matrix must be a numpy.ndarray")
+    my_len = matrix.shape[0]
+    if len(matrix.shape) != 2 or my_len != matrix.shape[1]:
+        return None
+    eVals, eVecs = np.linalg.eig(matrix)
+    if all(eVals > 0):
+        return "Positive definite"
+    if all(eVals >= 0):
+        return "Positive semi-definite"
+    if all(eVals < 0):
+        return "Negative definite"
+    if all(eVals <= 0):
+        return "Negative semi-definite"
+    else:
+        return "Indefinite"
