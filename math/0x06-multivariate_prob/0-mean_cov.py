69c577dddb1812222ef8f3383c14e2f524f51a44
GunterPearson <1715@holbertonschool.com>
2021-07-25 13:35:40 -0500

    pushing file 0-mean_cov.py

---
+++ math/0x06-multivariate_prob/0-mean_cov.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""mulitvaritive probability"""
+import numpy as np
+
+
+def mean_cov(X):
+    """that calculates the mean and covariance of a data set"""
+    if len(X.shape) != 2:
+        raise TypeError("X must be a 2D numpy.ndarray")
+    n, d = X.shape
+    if n < 2:
+        raise ValueError("X must contain multiple data points")
+    m = np.mean(X, axis=0, keepdims=True)
+    C = np.matmul((X - m).T, (X - m)) / (n - 1)
+    return m, C
