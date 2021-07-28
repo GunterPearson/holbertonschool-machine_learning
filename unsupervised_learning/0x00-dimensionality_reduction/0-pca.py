828ed52b8b1760cb8b438aa75fde58da93d45f29
GunterPearson <1715@holbertonschool.com>
2021-07-27 19:51:15 -0500

    pushing file 0-pca.py

---
+++ unsupervised_learning/0x00-dimensionality_reduction/0-pca.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+""" dimensionality reduction"""
+import numpy as np
+
+
+def pca(X, var=0.95):
+    """performs PCA on a dataset"""
+    # X is a (n x d) matrix (50, 6)
+    n, d = X.shape
+    K = 1
+    U, S, V = np.linalg.svd(X, full_matrices=True)
+    for x in range(d):
+        varian = (np.sum(S[:K]) / np.sum(S))
+        if varian >= var:
+            print(V[:K].shape)
+            return V[:K].T
+        K += 1
