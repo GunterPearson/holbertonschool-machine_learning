0684dedf4fa2547fcdc527c78a6314298a83531e
GunterPearson <1715@holbertonschool.com>
2021-08-04 01:59:16 -0500

    pushing file 4-initialize.py

---
+++ unsupervised_learning/0x01-clustering/4-initialize.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+""" clustering """
+import numpy as np
+kmeans = __import__('1-kmeans').kmeans
+
+
+def initialize(X, k):
+    """initializes variables for a Gaussian Mixture Model"""
+    # X.shape = (12500, 2)
+    # --- Select random points for centroids
+    # plt.scatter(X[:, 0], X[:, 1], s=10)
+    # plt.scatter(centroids[:, 0], centroids[:, 1], c='r', s=13)
+    # plt.show()
+    if type(X) is not np.ndarray or len(X.shape) != 2:
+        return None, None, None
+    if type(k) != int or k < 1:
+        return None, None, None
+    n, d = X.shape
+    pi = [1 / k for i in range(k)]
+    cen, classes = kmeans(X, k)
+    cov_mat = np.tile(np.identity(d), (k, 1, 1))
+    return pi, cen, cov_mat
