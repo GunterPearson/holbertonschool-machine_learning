d8fdc53359fa876f77c4a03c9d3b48484acdecf2
GunterPearson <1715@holbertonschool.com>
2021-08-25 11:42:42 -0500

    pushing updated 0-vanilla.py

--- unsupervised_learning/0x04-autoencoders/0-vanilla.py
+++ unsupervised_learning/0x04-autoencoders/0-vanilla.py
@@ -6,24 +6,21 @@ import tensorflow.keras as keras
 def autoencoder(input_dims, hidden_layers, latent_dims):
     """creates an autoencoder"""
     # autoencoder(784, [128, 64], 32)
-    encode = keras.Sequential()
-    encode.add(keras.layers.Dense(hidden_layers[0],
-                                  activation='relu', input_dim=input_dims))
+    input = keras.Input(shape=(input_dims,))
+    encoded1 = keras.layers.Dense(hidden_layers[0], activation='relu')(input)
     for lay in hidden_layers[1:]:
-        encode.add(keras.layers.Dense(lay, activation='relu'))
-    encode.add(keras.layers.Dense(latent_dims, activation='relu'))
+        encoded2 = keras.layers.Dense(lay, activation='relu')(encoded1)
+    encoded3 = keras.layers.Dense(latent_dims, activation='relu')(encoded2)
+    encoder = keras.Model(input, encoded3)
 
-    decode = keras.Sequential()
-    decode.add(keras.layers.Dense(hidden_layers[1],
-                                  activation='relu', input_dim=latent_dims))
-    for lay in hidden_layers[-2::-1]:
-        decode.add(keras.layers.Dense(lay, activation='relu'))
-    decode.add(keras.layers.Dense(input_dims, activation='sigmoid'))
+    input2 = keras.Input(shape=(latent_dims,))
+    decoded1 = keras.layers.Dense(hidden_layers[-1], activation='relu')(input2)
+    for dim in hidden_layers[-2::-1]:
+        decoded2 = keras.layers.Dense(dim, activation='relu')(decoded1)
+    decoded3 = keras.layers.Dense(input_dims, activation='sigmoid')(decoded2)
+    decoder = keras.Model(input2, decoded3)
 
-    auto = keras.Sequential()
-    auto.add(encode)
-    auto.add(decode)
-
+    auto = keras.Model(input, decoder(encoder(input)))
     auto.compile(loss='binary_crossentropy', optimizer='adam')
 
-    return encode, decode, auto
+    return encoder, decoder, auto
