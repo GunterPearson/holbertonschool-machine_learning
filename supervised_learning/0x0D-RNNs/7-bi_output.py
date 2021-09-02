cb5fa36f14ea156898532b7a66d60ff37d88104d
GunterPearson <1715@holbertonschool.com>
2021-09-02 09:26:34 -0500

    pushing file 7-bi_output.py

---
+++ supervised_learning/0x0D-RNNs/7-bi_output.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+"""RNN module"""
+import numpy as np
+
+
+class BidirectionalCell():
+    """represents a cell of a simple RNN"""
+    def __init__(self, i, h, o):
+        """class constructor"""
+        # 10, 15, 5 = (i, h, o)
+        self.Whf = np.random.randn(i + h, h)
+        self.Whb = np.random.randn(i + h, h)
+        self.Wy = np.random.randn(2 * h, o)
+
+        self.bhf = np.zeros((1, h))
+        self.bhb = np.zeros((1, h))
+        self.by = np.zeros((1, o))
+
+    def forward(self, h_prev, x_t):
+        """feed forward"""
+        # (8, 15) (8, 15) (8, 10)
+        h_x = np.concatenate((h_prev.T, x_t.T), axis=0)
+
+        h_next = np.tanh(h_x.T @ self.Whf + self.bhf)
+        return h_next
+
+    def backward(self, h_next, x_t):
+        """calculates the hidden state in the backward"""
+        h_x = np.concatenate((h_next.T, x_t.T), axis=0)
+
+        h_prev = np.tanh(h_x.T @ self.Whb + self.bhb)
+        return h_prev
+
+    def softmax(self, x):
+        """return softmax"""
+        y = np.exp(x) / np.sum(np.exp(x), -1, keepdims=True)
+        return y
+
+    def output(self, H):
+        """calculates all outputs for the RNN"""
+        y = self.softmax(H @ self.Wy + self.by)
+        return y
