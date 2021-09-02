e047c79de6091d4a1cbe705dbef8ef090062e961
GunterPearson <1715@holbertonschool.com>
2021-09-02 08:37:33 -0500

    pushing files 1-3

--- supervised_learning/0x0D-RNNs/0-rnn_cell.py
+++ supervised_learning/0x0D-RNNs/0-rnn_cell.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python3
-"""RNN module"""
+"""RNNs"""
 import numpy as np
 
 
-class RNNCell:
-    """RNNCell class
+class RNNCell():
+    """
+    represents a cell of a simple RNN
     """
     def __init__(self, i, h, o):
         """Initializer
+
         Arguments:
             i {int} -- Is dimensionality of the data
             h {int} -- Is dimensionality of hidden state
@@ -18,27 +20,32 @@ class RNNCell:
         self.bh = np.zeros((1, h))
         self.by = np.zeros((1, o))
 
-    @staticmethod
-    def softmax(z):
+    def softmax(self, x):
         """Calculates the softmax
+
         Arguments:
-            z {np.ndarray} -- Contains array input
+            x {np.ndarray} -- Contains array input
+
         Returns:
             np.ndarray -- Softmaxed numpy arrat
         """
-        ex = np.exp(z)
-        return ex / np.sum(ex, 1, keepdims=True)
+        y = np.exp(x) / np.sum(np.exp(x), axis=1, keepdims=True)
+        return y
 
     def forward(self, h_prev, x_t):
         """Performs forward propagation for one time step
+
         Arguments:
             h_prev {np.ndarrat} -- Contains the previous hidden state
             x_t {np.ndarray} -- Contains the data input of the cell
+
         Returns:
             tuple(np.ndarray) -- Contains the next hidden state, the output of
             the cell
         """
-        stacked = np.hstack((h_prev, x_t))
-        h_next = np.tanh(stacked @ self.Wh + self.bh)
-        y = self.softmax(h_next @ self.Wy + self.by)
+        one = np.concatenate((h_prev.T, x_t.T), axis=0)
+        two = np.matmul(one.T, self.Wh) + self.bh
+        h_next = np.tanh(two)
+        o = np.matmul(h_next, self.Wy) + self.by
+        y = self.softmax(o)
         return h_next, y
