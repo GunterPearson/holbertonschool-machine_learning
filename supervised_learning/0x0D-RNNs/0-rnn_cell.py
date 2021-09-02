1f167449a90b4d085f720a38021df2d426b8a90e
GunterPearson <1715@holbertonschool.com>
2021-09-02 08:31:18 -0500

    pushing files 1-3

--- supervised_learning/0x0D-RNNs/0-rnn_cell.py
+++ supervised_learning/0x0D-RNNs/0-rnn_cell.py
@@ -4,21 +4,29 @@ import numpy as np
 
 
 class RNNCell():
-    """represents a cell of a simple RNN"""
+    """
+    represents a cell of a simple RNN
+    """
     def __init__(self, i, h, o):
-        """class constructor"""
+        """
+        class constructor
+        """
         self.Wh = np.random.randn(i + h, h)
         self.Wy = np.random.randn(h, o)
         self.bh = np.zeros((1, h))
         self.by = np.zeros((1, o))
 
     def softmax(self, x):
-        """return softmax"""
+        """
+        return softmax
+        """
         y = np.exp(x) / np.sum(np.exp(x), axis=1, keepdims=True)
         return y
 
     def forward(self, h_prev, x_t):
-        """feed forward"""
+        """
+        feed forward
+        """
         one = np.concatenate((h_prev.T, x_t.T), axis=0)
         two = np.matmul(one.T, self.Wh) + self.bh
         h_next = np.tanh(two)
