e0358cb67ec28d30766fe4102365c6104e7c26bf
GunterPearson <1715@holbertonschool.com>
2021-09-28 12:34:05 -0500

    pushing file 11-transformer.py

---
+++ supervised_learning/0x11-attention/11-transformer.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""Transformer class"""
+import tensorflow as tf
+Encoder = __import__('9-transformer_encoder').Encoder
+Decoder = __import__('10-transformer_decoder').Decoder
+
+
+class Transformer(tf.keras.layers.Layer):
+    """Class to create the transformer network"""
+    def __init__(self, N, dm, h, hidden, input_vocab, target_vocab,
+                 max_seq_input, max_seq_target, drop_rate=0.1):
+        """Class constructor"""
+        super(Transformer, self).__init__()
+        self.encoder = Encoder(
+            N, dm, h, hidden, input_vocab, max_seq_input, drop_rate)
+        self.decoder = Decoder(
+            N, dm, h, hidden, target_vocab, max_seq_target, drop_rate)
+        self.linear = tf.keras.layers.Dense(units=target_vocab)
+
+    def call(self, inputs, target, training, encoder_mask, look_ahead_mask,
+             decoder_mask):
+        """Calls the transformer"""
+        encoder_output = self.encoder(inputs, training, encoder_mask)
+        decoder_output = self.decoder(target, encoder_output, training,
+                                      look_ahead_mask, decoder_mask)
+        result = self.linear(decoder_output)
+        return result
