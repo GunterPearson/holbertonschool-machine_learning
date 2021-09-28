e4148c26df464a5b2d9ddbedf6aa95822123a9aa
GunterPearson <1715@holbertonschool.com>
2021-09-28 11:39:19 -0500

    pushing file 0-rnn_encoder.py

---
+++ supervised_learning/0x11-attention/0-rnn_encoder.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""RNN Encoder"""
+import tensorflow as tf
+import numpy as np
+
+
+class RNNEncoder(tf.keras.layers.Layer):
+    """enocde layer inherits from keras layer"""
+    def __init__(self, vocab, embedding, units, batch):
+        """class constructor"""
+        super(RNNEncoder, self).__init__()
+        self.batch = batch
+        self.units = units
+        self.embedding = tf.keras.layers.Embedding(
+            vocab, embedding)
+        self.gru = tf.keras.layers.GRU(
+            self.units,
+            return_sequences=True,
+            return_state=True,
+            recurrent_initializer='glorot_uniform')
+
+    def initialize_hidden_state(self):
+        """Initializes hidden states RNN cell to a tensor of zeros"""
+        return tf.zeros(shape=(self.batch, self.units))
+
+    def call(self, x, initial):
+        """Returns: outputs, hidden"""
+        embedded = self.embedding(x)
+        return self.gru(inputs=embedded, initial_state=initial)
