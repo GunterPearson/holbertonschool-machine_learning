50dd0c12a355a030727f997eca6a5de39cda9ea2
GunterPearson <1715@holbertonschool.com>
2021-11-29 11:32:40 -0600

    pushing new file 0-passengers that pulls data from api and returns ship names that can hold a given number of passegers

---
+++ pipeline/0x01-apis/0-passengers.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""Pipeline Api"""
+import requests
+
+
+def availableShips(passengerCount):
+    """show all starhips avaliable from the API"""
+    url = "https://swapi-api.hbtn.io/api/starships"
+    r = requests.get(url)
+    ship_list = []
+    while r.status_code == 200:
+        for ship in r.json()["results"]:
+            if ship["passengers"] is not None:
+                try:
+                    if int(ship["passengers"]) >= passengerCount:
+                        ship_list.append(ship["name"])
+                except ValueError:
+                    pass
+        try:
+            r = requests.get(r.json()["next"])
+        except Exception:
+            break
+    return ship_list
