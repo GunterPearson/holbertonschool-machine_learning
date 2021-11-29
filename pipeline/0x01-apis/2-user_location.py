aaa921d7f0fe4e055765210bf6395c2ba14d71fc
GunterPearson <1715@holbertonschool.com>
2021-11-29 12:41:03 -0600

    pushing new file 2-user_location.py that returns location of user if user in Null returns not found and after reaching api limit returns wait time

---
+++ pipeline/0x01-apis/2-user_location.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""Pipeline Api"""
+import requests
+import sys
+from datetime import datetime
+
+url = sys.argv[1]
+response = requests.get(url)
+
+if response.status_code == 404:
+    print("Not found")
+elif response.status_code == 403:
+    date = datetime.fromtimestamp(int(response.headers['X-Ratelimit-Reset']))
+    min = str((date - datetime.now())).split(':')[1]
+    print("Reset in {} minutes".format(min))
+else:
+    print(response.json()["location"])
