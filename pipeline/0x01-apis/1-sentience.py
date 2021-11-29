dda39229b86dfa5a2634a9252de27b21851cc5c7
GunterPearson <1715@holbertonschool.com>
2021-11-29 12:06:01 -0600

    pushing new file 1-sentience.py that pulls data from api and returns home planets of species given

---
+++ pipeline/0x01-apis/1-sentience.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""Pipeline Api"""
+import requests
+
+
+def sentientPlanets():
+    """returns the list of names of the home planets of all sentient species"""
+    url = "https://swapi-api.hbtn.io/api/species"
+    r = requests.get(url)
+    world_list = []
+    while r.status_code == 200:
+        for species in r.json()["results"]:
+            if species["designation"] == "sentient":
+                url = species["homeworld"]
+                if url is not None:
+                    ur = requests.get(url)
+                    world_list.append(ur.json()["name"])
+        try:
+            r = requests.get(r.json()["next"])
+        except Exception:
+            break
+    return world_list
