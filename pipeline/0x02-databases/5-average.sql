332ab1f5e0fc04fb1368ead61b88facdaea9bc0c
GunterPearson <1715@holbertonschool.com>
2021-12-08 15:26:11 -0600

    pushing new file 5-average.sql that prints out temporary new column average along with average score from all values in table

---
+++ pipeline/0x02-databases/5-average.sql
@@ -0,0 +1,6 @@
+-- computes the score average of all records in the table second_table in your MySQL server.
+
+-- The result column name should be average
+-- The database name will be passed as an argument of the mysql command
+
+SELECT AVG(score) AS average FROM second_table;
