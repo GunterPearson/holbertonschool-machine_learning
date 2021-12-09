16e1d6538c2bbf1a5fffbc8990e99d956eaeec77
GunterPearson <1715@holbertonschool.com>
2021-12-09 15:31:59 -0600

    pushing file 13-uniq_users.sql that creates new user while keeping only unique emails

--- pipeline/0x02-databases/13-uniq_users.sql
+++ pipeline/0x02-databases/13-uniq_users.sql
@@ -6,6 +6,6 @@
 -- name, string (255 characters)
 CREATE TABLE IF NOT EXISTS users (
 	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-	email VARCHAR(255) NOT NULL,
+	email VARCHAR(255) NOT NULL UNIQUE,
     name VARCHAR(255)
 );
