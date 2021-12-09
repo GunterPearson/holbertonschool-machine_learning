7c8e8f3a70224dedfb3f5f4e995c79957510858d
GunterPearson <1715@holbertonschool.com>
2021-12-09 15:31:01 -0600

    pushing file 13-uniq_users.sql that creates new user while keeping only unique emails

---
+++ pipeline/0x02-databases/13-uniq_users.sql
@@ -0,0 +1,11 @@
+-- Write a SQL script that creates a table users following these requirements:
+
+-- With these three attributes:
+-- id, integer, never null, auto increment and primary key
+-- email, string (255 characters), never null and unique
+-- name, string (255 characters)
+CREATE TABLE IF NOT EXISTS users (
+	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	email VARCHAR(255) NOT NULL,
+    name VARCHAR(255)
+);
