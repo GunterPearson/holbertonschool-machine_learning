91ad780fb26a12e6d3ffa06d4a6775fc31be91c8
GunterPearson <1715@holbertonschool.com>
2021-12-09 14:50:36 -0600

    pushin file 9-no_genre that list all shows with no corelated genre

---
+++ pipeline/0x02-databases/9-no_genre.sql
@@ -0,0 +1,11 @@
+-- Write a script that lists all shows contained in hbtn_0d_tvshows without a genre linked.
+
+-- Each record should display: tv_shows.title - tv_show_genres.genre_id
+-- Results must be sorted in ascending order by tv_shows.title and tv_show_genres.genre_id
+-- You can use only one SELECT statement
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+LEFT JOIN tv_show_genres
+ON tv_shows.id = tv_show_genres.show_id
+WHERE tv_show_genres.genre_id IS NULL
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
