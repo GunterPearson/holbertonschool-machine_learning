cc48b2915274dea3f0071197fdc49d8dfb8b95e5
GunterPearson <1715@holbertonschool.com>
2021-12-13 11:15:53 -0600

    pushing new file 14-country_users.sql that creates table users with id, email, name, and country with defualt us

--- pipeline/0x02-databases/14-country_users.sql
+++ pipeline/0x02-databases/14-country_users.sql
@@ -4,8 +4,9 @@
 -- name, string (255 characters)
 -- country, enumeration of countries: US, CO and TN, never null (= default will be the first element of the enumeration, here US)
 CREATE TABLE IF NOT EXISTS users (
-  id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
-  email VARCHAR(255) NOT NULL UNIQUE,
-  name VARCHAR(255),
-  country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US'
+	id INT NOT NULL AUTO_INCREMENT UNIQUE,
+	email VARCHAR(255) NOT NULL UNIQUE,
+	name VARCHAR(255),
+	country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US',
+	PRIMARY KEY (id)
 );
